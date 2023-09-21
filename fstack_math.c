86d237851291add20c7e70fa7e7b30a80d4ab107
El Houssain Souhail <sou.ductive@gmail.com>
2023-09-22 00:07:42 +0100

    Fixed error message and checking for the math opcodes

--- fstack_math.c
+++ fstack_math.c
@@ -74,10 +74,10 @@ void divide(stack_t **stack, unsigned int line_number)
 {
 	if (!(*stack) || !((*stack)->next) || (*stack)->n == 0)
 	{
-		if ((*stack)->n == 0)
+		if ((*stack)->next && (*stack)->n == 0)
 			fprintf(stderr, "L%u: division by zero\n", line_number);
 		else
-			fprintf(stderr, "L%u: can't mul, stack too short\n", line_number);
+			fprintf(stderr, "L%u: can't div, stack too short\n", line_number);
 
 		fclose(file);
 		sfree(*stack);
@@ -99,10 +99,10 @@ void mod(stack_t **stack, unsigned int line_number)
 {
 	if (!(*stack) || !((*stack)->next) || (*stack)->n == 0)
 	{
-		if ((*stack)->n == 0)
+		if ((*stack)->next && (*stack)->n == 0)
 			fprintf(stderr, "L%u: division by zero\n", line_number);
 		else
-			fprintf(stderr, "L%u: can't mul, stack too short\n", line_number);
+			fprintf(stderr, "L%u: can't mod, stack too short\n", line_number);
 
 		fclose(file);
 		sfree(*stack);

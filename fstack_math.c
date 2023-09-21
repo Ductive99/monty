8401c6c112b7143a5a387868039b1cbe11ebfc11
El Houssain Souhail <sou.ductive@gmail.com>
2023-09-22 00:13:03 +0100

    fixed substraction order

--- fstack_math.c
+++ fstack_math.c
@@ -38,7 +38,7 @@ void sub(stack_t **stack, unsigned int line_number)
 		exit(EXIT_FAILURE);
 	}
 
-	(*stack)->next->n = (*stack)->n - (*stack)->next->n;
+	(*stack)->next->n -= (*stack)->n;
 	pop(stack, line_number);
 }
 
